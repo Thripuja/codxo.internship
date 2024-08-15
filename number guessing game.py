b5ac8d5f3df9d4faaf287323631df5a71a594166
Thripuja <thripuja.s@gmail.com>
2024-08-15 10:53:50 +0530

    Add files via upload

---
+++ number guessing game.py
@@ -0,0 +1,50 @@
+import random
+import math
+# Taking Inputs
+lower = int(input("Enter Lower bound:- "))
+
+# Taking Inputs
+upper = int(input("Enter Upper bound:- "))
+
+# generating random number between
+# the lower and upper
+x = random.randint(lower, upper)
+print("\n\tYou've only ",
+	round(math.log(upper - lower + 1, 2)),
+	" chances to guess the integer!\n")
+
+# Initializing the number of guesses.
+count = 0
+
+# for calculation of minimum number of
+# guesses depends upon range
+#So, the minimum number of guesses depends upon range.
+#And the compiler must calculate the minimum number of guessing
+#depends upon the range, on its own.
+#For this, we have a formula:-
+#Minimum number of guessing = log2(Upper bound – lower bound + 1)
+
+while count < math.log(upper - lower + 1, 2):
+	count += 1
+
+	# taking guessing number as input
+	guess = int(input("Guess a number:- "))
+
+	# Condition testing
+	if x == guess:
+		print("Congratulations you did it in ",
+			count, " try")
+		# Once guessed, loop will break
+		break
+	elif x > guess:
+		print("You guessed too small!")
+	elif x < guess:
+		print("You Guessed too high!")
+
+# If Guessing is more than required guesses,
+# shows this output.
+if count >= math.log(upper - lower + 1, 2):
+	print("\nThe number is %d" % x)
+	print("\tBetter Luck Next time!")
+
+# Better to use This source Code on pycharm!
